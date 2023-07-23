6c6eeb9328bfaef9ebcd68665046495475bc9675
TheDoritoboy <doritoboy.ekk@gmail.com>
2023-07-23 15:44:29 -0700

    hatred

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from '../componants/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div className= "App">
+        <Header />
         <h1>Hello there</h1>
       </div>
     
