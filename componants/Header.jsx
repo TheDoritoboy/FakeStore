16b928c2fab48adcdf9a39fb408162511d5425c4
Kenny Kruger <doritoboy.ekk@gmail.com>
2023-07-23 16:18:08 -0700

    beer

--- componants/Header.jsx
+++ componants/Header.jsx
@@ -1,10 +1,25 @@
 
 import React from 'react'
+import { FaBeer } from 'react-icons/fa';
 
 function header() {
   return (
-    <div>header</div>
+    <div>
+      <h3> Lets go for a <FaBeer />? </h3>
+    </div>
+    
+    
   )
 }
-
+class Question extends React.Component {
+  render() {
+    return <h3> Lets go for a <FaBeer />? </h3>
+  }
+  header() {
+    return (
+      <div>header</div>
+      
+    )
+  }
+}
 export default header; 
