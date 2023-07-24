50bae3c4028a2d9db397e61bd0cf3e9e452e0c7e
TheDoritoboy <doritoboy.ekk@gmail.com>
2023-07-23 17:29:47 -0700

    some format things

---
+++ pages/Homepage.jsx
@@ -0,0 +1,29 @@
+import React {useEffect, useState} from 'react'
+import '../src/homepage.css'
+function Homepage() {
+    const 
+  return (
+    <div>
+        <div>
+            <button>All</button>
+            <button>Electronics</button>
+            <button>Jewelery</button>
+            <button>Men's Clothing</button>
+            <button>Woman's Closthing</button>
+        </div>
+        <div className='grid-container'>
+                <div className='grid-item'>
+                    <img src="" alt="" />
+                    <p className='name'></p>
+                    <p className='info'></p>
+                    <p className='price'></p>
+                </div>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Homepage
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../componants/header'
+import Homepage from '../pages/Homepage'
+Homepage
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div className= "App">
         <Header />
-        <h1>Hello there</h1>
+        <Homepage />
       </div>
     
   )
