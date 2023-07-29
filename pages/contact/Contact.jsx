42532768982d879ef71b4dbe3814e2703a765fd2
Kenny <doritoboy.ekk@gmail.com>
2023-07-29 15:52:08 -0700

    added routes

---
+++ pages/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+        <h1>Contact Us</h1>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Contact
--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { useState } from 'react'
 import './App.css'
 import Header from '../componants/header'
 import Homepage from '../pages/Homepage'
-import Footer from './../componants/ItemCards/Footer/Footer';
+import Footer from '../componants/Footer/Footer';
+import {BroswerRouter, Routes, Route, BrowserRouter} from 'react-router-dom'
+import Details from '../pages/details/Details';
+import Contact from '../pages/contact/Contact';
 Homepage
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className= "App">
-        <Header />
-        <Homepage />
-        <Footer />
-      </div>
+      <BrowserRouter>
+          <Header />
+
+          <Routes>
+            <Route path='/' element= {<Homepage />}/>
+            <Route path='/details' element= {<Details />}/>
+            <Route path='/contact' element= {<Contact />}/>
+          </Routes>
+
+          <Footer />
+      </BrowserRouter>
     
   )
 }
