a3c95d592902570b246ed8b66643b8084909396b
Kenny <doritoboy.ekk@gmail.com>
2023-08-04 14:55:26 -0700

    added favorite button

--- componants/ItemCards/ItemCard.jsx
+++ componants/ItemCards/ItemCard.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import './ItemCard.css'
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+//AiOutlineHeart
+//AiFillHeart
 function ItemCard({item}) {
+
+  const [textState, setTextState] = useState(<AiOutlineHeart />);
+
+  const toggleText = () => {
+    setTextState((state) => (state === <AiFillHeart /> ? <AiOutlineHeart /> : <AiFillHeart />));
+};
+
+
   return (
     <div className='item-card'>
+       <button onClick={toggleText} className='heart'>{textState}</button>
       <img src={item.image} alt="goods" />
       <a href= {`/details/${item.id}`} className='title'>{item.title}</a>
       <p className='category'>{item.category}</p>
